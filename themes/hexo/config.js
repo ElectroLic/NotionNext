9ca0e09f9871e7e40c44e96ff6c4c42c5e8b7bb6
ElectroLic <149926240+ElectroLic@users.noreply.github.com>
2023-11-05 22:59:08 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   HOME_BANNER_ENABLE: true,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
-  HOME_BANNER_GREETINGS: ['Emm...朋友你好鸭', 'Emm...是迷路了吗', '要不先进来看看吧O(∩_∩)O'], // 首页大图标语文字
+  HOME_BANNER_GREETINGS: ['Hi,我是一个程序员', 'Hi,我是一个打工人', 'Hi,我是一个干饭人'], // 首页大图标语文字
 
   HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
   // 已知未修复bug, 在移动端开启true后会加载不出图片； 暂时建议设置为false。
